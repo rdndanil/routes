284bd9c18ed10c4a0f78e1f44fc265808a565c3b
twnlka <rodindanil4@gmail.com>
2025-04-12 00:04:28 +0500

    Add GitHub Pages deploy config

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import MainPage from "./pages/MainPage";
 import ZayavkiPage from "./pages/ZayavkiPage";
 
 const App = () => {
   return (
-    <Router basename="/routes">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/zayavki" element={<ZayavkiPage />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
