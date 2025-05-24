b03f9a858e82713fbcdaf103b1beedfc4be87ea0
twnlka <rodindanil4@gmail.com>
2025-05-24 23:42:08 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import MainPage from "./pages/MainPage";
 import ZayavkiPage from "./pages/ZayavkiPage";
