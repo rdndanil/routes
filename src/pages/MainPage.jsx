2ce205aea8c956a6a8a0dd9dfbebce15e26e81fa
twnlka <rodindanil4@gmail.com>
2025-05-24 23:26:43 +0500

    Update MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -66,8 +66,8 @@ const MainPage = () => {
             </div>
 
             {/* Карточка 2 */}
-            <div className="bg-gray-400 dark:bg-gray-600 text-gray-500 shadow-lg rounded-lg p-6">
-              <h3 className="text-lg font-semibold mb-4 text-gray-800 dark:text-white flex items-center">
+            <div className="bg-blue-600 dark:bg-blue-700 text-white shadow-lg rounded-lg p-6">
+              <h3 className="text-lg font-semibold mb-4 text-white flex items-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -79,20 +79,20 @@ const MainPage = () => {
                   <path
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                    d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636"
+                    d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z"
                   />
                 </svg>
-                Недоступно
+                Норматив движения ТС
               </h3>
-              {/* <p className="mt-2 text-sm">
-                Анализируйте направления грузов и их распределение по складам.
+              <p className="mt-2 text-sm">
+                Рассчитайте примерное время ТС в пути, предварительно указав маршрут, дату и время отправления ТС.
               </p>
               <button
-                disabled
-                className="mt-4 bg-gray-300 text-gray-600 font-medium py-2 px-4 rounded cursor-not-allowed"
+                onClick={() => navigate("/normativ")} // Переход на страницу Нормативы
+                className="mt-4 bg-white text-blue-600 font-medium py-2 px-4 rounded hover:bg-gray-100"
               >
-                Недоступно
-              </button> */}
+                Перейти
+              </button>
             </div>
 
             {/* Карточка 3 */}
