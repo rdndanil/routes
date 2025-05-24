9c121a5c816e2c0cc46a8029810250adcbd769f9
twnlka <rodindanil4@gmail.com>
2025-05-24 23:21:16 +0500

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -62,6 +62,27 @@ const Sidebar = () => {
           </svg>
             Выгрузка
           </Link>
+
+                    <Link
+            to="/normativ" // Указываем путь для перехода (например, на страницу "Выгрузка")
+            className="hover:text-indigo-400 flex items-center gap-x-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 mr-1"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            Нормативы движения
+          </Link>
           {/* <Link
             to="/zayavki"
             className="hover:text-indigo-400 flex items-center gap-x-2"
